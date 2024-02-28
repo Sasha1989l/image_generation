880bd6be9f6844016f1b77a6d496400b0e38c9dc
Alexander <kzsasha1989@gmail.com>
2024-02-28 11:45:39 +0300

    init app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Card, InputGroup, Form, FloatingLabel} from "react-bootstrap";
+import {useEffect} from "react";
+import OpenAI from "openai";
+
 
 function App() {
+  const openai = new OpenAI({
+    apiKey: process.env.REACT_APP_OPEN_AI_KEY,
+    dangerouslyAllowBrowser: true
+  });
+
+
+  useEffect(() => {
+    console.log(process.env.NODE_ENV)
+    console.log(`Open ai key: ${process.env.REACT_APP_OPEN_AI_KEY}`)
+  }, []);
+
+  const generate_image = async () => {
+    // const image = await openai.images.generate({ prompt: "A cute baby sea otter" });
+    // console.log(image.data);
+
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: "Say this is a test" }],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(chatCompletion)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card style={{ width: '25rem' }} className="mx-auto my-3">
+        <Card.Img variant="top" src="https://i.ytimg.com/vi/QE_AiPrRHZs/maxresdefault.jpg" />
+        <Card.Body className="text-center">
+          <InputGroup className="mb-3">
+            <FloatingLabel label="Промпт">
+              <Form.Control as="textarea" aria-label="Prompt" style={{ height: '150px' }} />
+            </FloatingLabel>
+          </InputGroup>
+          <Button variant="primary" onClick={ generate_image }>Сгенерировать</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
